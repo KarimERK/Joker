5590102c6bea3de37e2f43b96a65890989a8bd95
KarimERK <kamelk@sheridancollege.ca>
2022-02-11 13:57:04 -0500

    Declared the cards values

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -9,6 +9,18 @@ package blackjack;
 //Declaration for the class
 public class Card {
     
-  
+  //Declaring value of cards
+    //52 cards in the deck
+    //All cards have suit and value
+    
+    public enum Value{
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, 
+        EIGHT, NINE, TEN, JACK, QUEEN, KING;
+        //Method that used for accessing the values of the numiration 
+        public static Value getValues(int points) {
+            //Array that holds the values for all numirations above
+            Value[] values = Value.values();
+            return values[points];
+        }
 
 }
