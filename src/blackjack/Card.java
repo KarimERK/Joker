037354f74a0a007b77646c8c7e4fe05aa50fa025
KarimERK <kamelk@sheridancollege.ca>
2022-02-11 14:19:10 -0500

    Some arrangement to give the code good smell

--- src/blackjack/Card.java
+++ src/blackjack/Card.java
@@ -6,58 +6,60 @@ package blackjack;
  * @author karimkamel
  */
 
-//Declaration for the class
+        //Declaration for the class
 public class Card {
     
- //Declaring value of cards
-    //52 cards in the deck
-    //All cards have suit and value
+        //Declaring value of cards
+        //52 cards in the deck
+        //All cards have suit and value
     
     public enum Value{
         ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, 
         EIGHT, NINE, TEN, JACK, QUEEN, KING;
+        
         //Method that used for accessing the values of the numiration 
         public static Value getValues(int points) {
-            //Array that holds the values for all numirations above
+            
+        //Array that holds the values for all numirations above
             Value[] values = Value.values();
             return values[points];
         }
       
-
         //Return the link to all these numirations for 13 values
         public static int getValueLenght(){
             return Value.values().length;
-  
         }
         
     }
-    //Second numiration
+        //Second numiration
     public enum Suit{
         HEART, DIAMOND, CLUB, SPADE;
         
         //Method that used for accessing the suit of the numiration 
         public static Suit getSuites(int points) {
+            
             //Array that holds the suits for all numirations above
             Suit[] suits = Suit.values();
             return suits[points];
         }
+        
         //Return the link to all these numirations for 4 suits
         public static int getSuitLenght(){
             return Suit.values().length;
-            
         }
+        
     }
-    //Creating attributes of the class
+    
+        //Creating attributes of the class
     private Suit suit;
     private Value value;
     
-    //Creating constructor for the class
+        //Creating constructor for the class
     public Card(Suit suit, Value value) {
         this.suit = suit;
         this.value = value;
-
     }
-    // Generating Getters and Setters
+        // Generating Getters and Setters
     public Suit getSuit() {
         return suit;
     }
@@ -74,13 +76,11 @@ public class Card {
         this.value = value;
     }
   
-    //toString method used to show how the card would look like when calling it
-    //For example: Club Seven, Hert King, Diamond Ace, Spade queen
+        //toString method used to show how the card would look like when calling it
+        //For example: Club Seven, Hert King, Diamond Ace, Spade queen
     public String toString() {
         return this.suit + "_" + this.value;
     }
-            
-
 }
 
 
