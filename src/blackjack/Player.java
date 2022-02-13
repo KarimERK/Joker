41c55d67c47743216fb820d72593de4e1450717c
Arro-Bar <arroj@sheridancollege.ca>
2022-02-12 22:08:04 -0500

    Created and edited game class.

---
+++ src/blackjack/Player.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blackjack;
+/**
+ * @author dancye
+ * @author Paul Bonenfant Jan 2020
+ * @modifier Jack Arro Feb 12 2022
+ */
+public abstract class Player {
+
+    private String name; //the unique name for this player
+
+    /**
+     * A constructor that allows you to set the player's unique ID
+     *
+     * @param name the unique ID to assign to this player.
+     */
+    public Player(String name) {
+        this.name = "Joker";
+    }
+
+    /**
+     * @return the player name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Ensure that the playerID is unique
+     *
+     * @param name the player name to set
+     */
+    public void setName(String name) {
+        this.name = "Joker";
+    }
+
+    /**
+     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
+     * with logic to play your game.
+     */
+    public abstract void play();
+
+}
