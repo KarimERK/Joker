cfbc12e001154d3499b2402ba43fcbbc2819f132
Arro-Bar <arroj@sheridancollege.ca>
2022-02-12 22:11:11 -0500

    Added additional phrases for the dealer.

--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -5,10 +5,23 @@ package blackjack;
  */
 public class Dealer {
     String name = "Dan";
-    Deck deck = new Deck();
     
     public void startGame(){
-        System.out.println("Hello! My name is " + name + ", and I will be your dealer in black jack today!");
+        System.out.println("Hello! My name is " + name + ", and I will be your dealer in blackjack today!");
+        System.out.print("Would you like to play a game of blackjack?(Say true if yes, and false if no!): ");
+    }
+    
+    public void play(){
+        System.out.println("Great! Let's play!");
+    }
+    
+    public void noPlay(){
+        System.out.println("No problem! Come back anytime!");
+        System.exit(0);
+    }
+    
+    public void playerMove(){
+        System.out.println("Would you like to hit or stand?(Say true if hit, and false if stand!): ");
     }
     
     public void playerLose(){
@@ -20,13 +33,11 @@ public class Dealer {
     }
     
     public void newRound(){
-        System.out.println("Would you like to play another round?");
+        System.out.print("Would you like to play another round?(Say true if yes, and false if no!): ");
     }
     
     public void endGame(){
-        System.out.println("Thank you for playing!");
+        System.out.println("Thank you for playing! Goodbye!");
         System.exit(0);
     }
-    
-    
 }
