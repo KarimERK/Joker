13f6cf9e64c573a0d92d1b853912c681e211ea5d
Arro-Bar <arroj@sheridancollege.ca>
2022-02-12 22:15:20 -0500

    Implemented dealer phrases and game loop.

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -1,16 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package blackjack;
+import java.util.Scanner;
 /**
  *
  * @author karimemil
- * @modified Jack Arro Feb 2022
+ * @modified Jack Arro Feb 12 2022
  */
 public class BlackJack {
     public static void main(String[] args) {
         
-    }
+        //Players input
+        Scanner input = new Scanner(System.in);
+        
+        //Create dealer/ Welcome/ Ask to play
+        Dealer dan = new Dealer();
+        dan.startGame();
+        
+        //If true play & If else quit
+        boolean play = input.nextBoolean();
+        if(play == true){
+            dan.play();
+        }
+        else{
+            dan.noPlay();
+        }
+        
+        //Game loop
+        while(0 != 1){
 
+            //***GAME CODE GOES HERE***
+            
+            //Request to play another round
+            dan.newRound();
+            
+            //If true play & If else quit
+            boolean newPlay = input.nextBoolean();
+            if(newPlay == true){
+                dan.play();
+            }
+            else{
+                dan.endGame();
+            }
+        }
+    }
 }
